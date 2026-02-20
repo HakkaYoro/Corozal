a51bae778fa0ef28f68fdc98f41bd75db1852b47
remumadchen <remumadchen@gmail.com>
2026-02-20 10:31:02 -0500

    BIG UPDATE!!!!!!!

---
+++ src/client/java/corozal/CorozalDataGenerator.java
@@ -0,0 +1,11 @@
+package corozal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CorozalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
